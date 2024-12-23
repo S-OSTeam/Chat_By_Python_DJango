22e165e9408589066165c03698a28d5db215f8f0
chanjin.k <chanjin.k@skuniv.ac.kr>
2024-12-23 14:58:18 +0900

    connect socket
    connect socket

---
+++ a_rtchat/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import *
+
+
+@csrf_exempt
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatmessageCreateForm()
+
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message': message,
+                'user': request.user
+            }
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context)
+
+    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages, 'form': form})
