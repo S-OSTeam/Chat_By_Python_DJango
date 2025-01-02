0f710644923e80f1fc15ae3252e82716087854af
chanjin.k <chanjin.k@skuniv.ac.kr>
2025-01-02 10:29:58 +0900

    feat: send file
    feat: send file

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,4 +1,7 @@
+import os
+
 import shortuuid
+from PIL import Image
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -18,11 +21,31 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def filename(self):
+        if self.file is not None:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+
     def __str__(self):
-        return f'{self.author.username} : {self.author}'
+        if self.body is not None:
+            return f'{self.author.username} : {self.body}'
+        elif self.file is not None:
+            return f'{self.author.username} : {self.filename}'
 
     class Meta:
         ordering = ['-createdAt']
+
+    @property
+    def is_image(self):
+        try:
+            image = Image.open(self.file)
+            image.verify()
+            return True
+        except Exception:
+            return False
